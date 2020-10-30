d72ecd3295069e9902477f3c981c259673308d94
Mohamed Hassan <mohamedhdesign@gmail.com>
2020-10-29 21:29:28 -0700

    Choose out Oct 30th - Website v1.2

--- dist/js/choose.js
+++ dist/js/choose.js
@@ -1,4 +1,23 @@
 $(window).on("load",function() {
+    if($(".social-section .posts").children().length == 0) {
+        $(".social-loader").show();
+    }
+    $.ajax({
+        url: "https://www.instagram.com/explore/tags/smoothdontmiss/?__a=1", 
+        success: function(data){
+            var posts = data.graphql.hashtag.edge_hashtag_to_media.edges;
+            console.log(data.graphql.hashtag);
+            posts.forEach(node => {
+                $(".social-loader").hide();
+                $(".social-section .posts").prepend("<a href='https://www.instagram.com/p/" + node.node.shortcode + "' target='_blank'><li><img src=" + node.node.thumbnail_src + " alt='instagram' /></li></a>");
+                // if (node.node.is_video) {
+                // } else {
+
+                // }
+            });
+        }
+    });
+
     $(window).scroll(function() {
         var windowBottom = $(this).scrollTop() + $(this).innerHeight();
         $(".fade").each(function() {
@@ -13,7 +32,14 @@ $(window).on("load",function() {
             }
         });
         // console.log(Math.max((Math.min(1 - $(window).scrollTop()*0.004 + 0.1, 1)), 0));
-        $(".action").css({"bottom": windowBottom/80 + 12 + "%", "opacity": Math.max((Math.min(1 - $(window).scrollTop()*0.004 + 0.1, 1)), 0)});
+        if($(window).width() > 700) {
+            $(".action").css({"bottom": windowBottom/80 + 12 + "%", "opacity": Math.max((Math.min(1 - $(window).scrollTop()*0.004 + 0.1, 1)), 0)});
+        }
+        if ($(window).scrollTop() > 360) {
+            $(".action").hide();
+        } else {
+            $(".action").show();
+        }
     }).scroll(); //invoke scroll-handler on page-load
     if ($(window).width() < 500) {
         $(".mobile-menu-btn").on("touch, click", function() {
